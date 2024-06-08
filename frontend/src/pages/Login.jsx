1a8022cbf06df73f50c5430ee0cd2b56b85a2218
Diego Salazar <diego.salazar2101@alumnos.ubiobio.cl>
2024-06-08 00:37:41 -0400

    ADD function submit for login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,34 +1,46 @@
-import React from "react";
-import Form from "../components/Form";
-import ImgLogo from "../components/ImgLogo";
+import { useNavigate } from 'react-router-dom';
+import { login } from '../services/auth.service.js';
+import Form from '../components/Form';
+import ImgLogo from '../components/ImgLogo';
 
 const Login = () => {
-	return (
-		<main className="container">
-			<ImgLogo />
-			<Form
-				title="Iniciar sesión"
-				fields={[
-					{
-						name: "email",
-						placeholder: "Ingrese su correo electrónico",
-						type: "email",
-					},
-					{
-						name: "password",
-						placeholder: "Ingrese su contraseña",
-						type: "password",
-					},
-				]}
-				buttonText="Iniciar sesión"
-				footerContent={
-					<p>
-						¿No tienes cuenta?, <a href="/register">Regístrate aquí!</a>
-					</p>
-				}
-			/>
-		</main>
-	);
+    const navigate = useNavigate();
+
+    const loginSubmit = (data) => {
+        login(data).then(() => {
+            navigate('/home')
+        })
+    }
+
+    return (
+        <main className="container">
+            <ImgLogo />
+            <Form
+                title="Iniciar sesión"
+                fields={[
+                    {
+                        name: "email",
+                        placeholder: "Ingrese su correo electrónico",
+                        type: "email",
+                        required: true,
+                    },
+                    {
+                        name: "password",
+                        placeholder: "Ingrese su contraseña",
+                        type: "password",
+                        required: true,
+                    },
+                ]}
+                buttonText="Iniciar sesión"
+                onSubmit={loginSubmit}
+                footerContent={
+                    <p>
+                        ¿No tienes cuenta?, <a href="/register">Regístrate aquí!</a>
+                    </p>
+                }
+            />
+        </main>
+    );
 };
 
 export default Login;
