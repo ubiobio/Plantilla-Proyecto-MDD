954621f381a5cce2a3510f590e2455e8fe709d6f
Diego Salazar <diego.salazar2101@alumnos.ubiobio.cl>
2024-06-08 00:35:20 -0400

    DEL import express

--- frontend/src/pages/EditUser.jsx
+++ frontend/src/pages/EditUser.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Form from "../components/Form";
 import Navbar from "../components/Navbar";
 
--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const Error404 = () => {
   console.log("Error 404, Page not found");
@@ -6,8 +5,8 @@ const Error404 = () => {
     <main className="error_404">
       <div className="card">
         <h1>404</h1>
-        <h3>~ Page not found ~</h3>
-        <h4>Sorry, the page you are looking for does not exist :(</h4>
+        <h3>~ Página no encontrada ~</h3>
+        <h4>Lo sentimos, la página que estás buscando no existe :(</h4>
       </div>
     </main>
   );
