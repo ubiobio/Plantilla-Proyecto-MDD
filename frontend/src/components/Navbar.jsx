3454ef3038a507537382ad4a8f0ecda1f662120b
Diego Salazar <diego.salazar2101@alumnos.ubiobio.cl>
2024-06-08 00:31:59 -0400

    ADD function submit for logout and event onClick

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,37 +1,43 @@
-import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { logout } from '../services/auth.service'; // Asegúrate de ajustar la ruta según tu estructura de proyecto
 
 const Navbar = () => {
-	const location = useLocation();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-	return (
-		<nav className="navbar">
-			<ul>
-				<li>
-					<img
-						src="cohete.png"
-						alt="Logo metodología de desarrollo"
-					/>
-				</li>
-				<li className={location.pathname === "/inicio" ? "active" : ""}>
-					<NavLink to="/home">Inicio</NavLink>
-				</li>
-				<li
-					className={
-						location.pathname === "/usuarios" ? "active" : ""
-					}
-				>
-					<NavLink to="/users">Usuarios</NavLink>
-				</li>
-				<li className={location.pathname === "/perfil" ? "active" : ""}>
-					<NavLink to="/profile">Perfil</NavLink>
-				</li>
-				<li className={location.pathname === "/" ? "active" : ""}>
-					<NavLink to="/">Cerrar</NavLink>
-				</li>
-			</ul>
-		</nav>
-	);
+    const logoutSubmit = () => {
+        try {
+            logout();
+            navigate('/'); 
+        } catch (error) {
+            console.error('Error al cerrar sesión:', error);
+        }
+    };
+
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <img
+                        src="cohete.png"
+                        alt="Logo metodología de desarrollo"
+                    />
+                </li>
+                <li className={location.pathname === "/inicio" ? "active" : ""}>
+                    <NavLink to="/home">Inicio</NavLink>
+                </li>
+                <li className={location.pathname === "/usuarios" ? "active" : ""}>
+                    <NavLink to="/users">Usuarios</NavLink>
+                </li>
+                <li className={location.pathname === "/perfil" ? "active" : ""}>
+                    <NavLink to="/profile">Perfil</NavLink>
+                </li>
+                <li className={location.pathname === "/" ? "active" : ""}>
+                    <NavLink to="/" onClick={logoutSubmit}>Cerrar</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
