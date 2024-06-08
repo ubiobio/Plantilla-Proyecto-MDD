1370d461b53506501b2bd2aa8c1a6c658a553e4f
Diego Salazar <diego.salazar2101@alumnos.ubiobio.cl>
2024-06-08 00:45:53 -0400

    DEL import login, register, EditUser, Navbar and profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
 // import './App.css'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppRouter from "./AppRouter";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import EditUser from "./pages/EditUser";
-import Navbar from "./components/Navbar";
-import Profile from "./pages/Profile";
+
 
 function App() {
   return (
