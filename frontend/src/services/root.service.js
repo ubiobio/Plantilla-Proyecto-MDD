e7a298766a0fd600bb42bbf814221a56ad37dcc4
Diego Salazar <diego.salazar2101@alumnos.ubiobio.cl>
2024-06-08 00:51:23 -0400

    ADD config Axios instance

---
+++ frontend/src/services/root.service.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000/api';
+
+const instance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+export default instance;
