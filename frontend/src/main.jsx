7595fa989ada4197515d6d6ee6db99ac23fd1f48
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-06-11 04:23:56 -0400

    DEL react strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-// import './index.css'
 import "./styles/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </BrowserRouter>
 );
