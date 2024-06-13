737be17c977e2d72ce4eec181a6050e3faeb95ec
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-06-13 01:59:49 -0400

    ADD user implementation filtered by rut, get all users and icons to edit and delete user

---
+++ frontend/src/pages/Users.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar';
+import Table from '../components/Table';
+import { getUsers, deleteUser } from '../services/user.service';
+import searchIcon from '../assets/searchIcon.svg'; // Asegúrate de que la ruta sea correcta
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const columns = ['Nombre', 'Rut', 'Correo', 'Rol', 'Acción'];
+
+  const dataUser = async () => {
+    try {
+      const response = await getUsers();
+      const formattedData = response.data.map(user => ({
+        Nombre: user.username,
+        Rut: user.rut,
+        Correo: user.email,
+        Rol: user.roles[0].name
+      }));
+      setUsers(formattedData);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
+
+  const handleDelete = async (rut) => {
+    try {
+      await deleteUser(rut);
+      setUsers(users.filter(user => user.Rut !== rut));
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    dataUser();
+  }, []);
+
+  const filteredUsers = users.filter(user =>
+    user.Rut.includes(searchTerm)
+  );
+  
+
+  return (
+    <>
+      <Navbar />
+      <div className='main-container'>
+        <div className='table-container'>
+          <div className='search-container'>
+            <div className='search-input-wrapper'>
+              <img src={searchIcon} alt="Buscar" className='search-icon' />
+              <input
+                type="text"
+                placeholder="Buscar usuario por rut"
+                value={searchTerm}
+                onChange={handleSearch}
+                className='search-input'
+              />
+            </div>
+          </div>
+          <Table columns={columns} data={filteredUsers} onDelete={handleDelete} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Users;
+
