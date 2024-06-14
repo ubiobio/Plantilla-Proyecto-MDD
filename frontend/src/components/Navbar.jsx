9c925105d5131452d485b753a025eeba6d821ef3
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-06-13 20:38:20 -0400

    MOD validate rol user

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
-import { logout } from '../services/auth.service'; // Asegúrate de ajustar la ruta según tu estructura de proyecto
+import { logout } from '../services/auth.service.js';
 
 const Navbar = () => {
     const location = useLocation();
     const navigate = useNavigate();
+
+    const storedUser = JSON.parse(sessionStorage.getItem('usuario'));
+    const userRole = storedUser?.data?.rolName;
 
     const logoutSubmit = () => {
         try {
@@ -19,16 +22,18 @@ const Navbar = () => {
             <ul>
                 <li>
                     <img
-                        src="cohete.png"
+                        src="/cohete.png"
                         alt="Logo metodología de desarrollo"
                     />
                 </li>
                 <li className={location.pathname === "/inicio" ? "active" : ""}>
                     <NavLink to="/home">Inicio</NavLink>
                 </li>
-                <li className={location.pathname === "/usuarios" ? "active" : ""}>
-                    <NavLink to="/users">Usuarios</NavLink>
-                </li>
+                {userRole === 'administrador' && (
+                    <li className={location.pathname === "/usuarios" ? "active" : ""}>
+                        <NavLink to="/users">Usuarios</NavLink>
+                    </li>
+                )}
                 <li className={location.pathname === "/perfil" ? "active" : ""}>
                     <NavLink to="/profile">Perfil</NavLink>
                 </li>
